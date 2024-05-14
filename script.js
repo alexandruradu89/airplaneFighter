42ebecad28c624e926c4f02df53ca0ba1be3bad3
alexandruradu89 <89183567+alexandruradu89@users.noreply.github.com>
2024-05-14 19:20:51 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
- let planeColumn = 3;
- let gameOver = false;
- let score = 0;
- let cyclicalFunctionsId = [];
- const boardWidth = 7, boardHeight = 6;
- const matrix = Array.from({length: boardHeight}, () =>
-                new Array(boardWidth).fill(0));
-         
- function createTable() {
+let planeColumn = 3;
+let gameOver = false;
+let score = 0;
+let cyclicalFunctionsId = [];
+const boardWidth = 7,
+    boardHeight = 6;
+const matrix = Array.from({length: boardHeight}, () =>
+    new Array(boardWidth).fill(0));
+
+function createTable() {
     const displayTable = document.createElement("table");
     displayTable.setAttribute("style", "border: 1px solid black");
     const tableBody = document.createElement("tbody");
@@ -19,58 +20,63 @@
             row.appendChild(cell);
         }
         tableBody.appendChild(row);
-        }
+    }
     displayTable.appendChild(tableBody);
     document.body.appendChild(displayTable);
     moveplane();
     shoot();
- }
+}
 
- function resetTable() {
-    for (let i  = 0; i < boardHeight; ++i) { 
+function resetTable() {
+    for (let i = 0; i < boardHeight; ++i) {
         for (let j = 0; j < boardWidth; ++j) {
             matrix[i][j] = 0;
         }
-    }   
- }
+    }
+}
 
- function resetWinnerMessage() {
+function resetWinnerMessage() {
     const message = document.getElementById("winnerMessage");
     message.innerHTML = "";
- }
+}
 
- function setColors() {
+function setColors() {
     for (let i = 0; i < boardHeight; ++i) {
         for (let j = 0; j < boardWidth; ++j) {
             let cellId = i + "-" + j;
             let currentCell = document.getElementById(cellId);
-            currentCell.setAttribute("style", "background-color: white; width: 30px; height: 30px");
+            currentCell.setAttribute("style",
+                "background-color: white; width: 30px; height: 30px");
             if (matrix[i][j] == 1) {
-                currentCell.setAttribute("style", "background-color: blue;  width: 30px; height: 30px");
+                currentCell.setAttribute("style",
+                    "background-color: blue;  width: 30px; height: 30px");
             }
             if (matrix[i][j] == 2) {
-                currentCell.setAttribute("style", "background-color: red;  width: 30px; height: 30px");
+                currentCell.setAttribute("style",
+                    "background-color: red;  width: 30px; height: 30px");
             }
             if ((i == boardHeight - 1) && (j == planeColumn)) {
-                currentCell.setAttribute("style", "background-color: green;  width: 30px; height: 30px");
+                currentCell.setAttribute("style",
+                    "background-color: green;  width: 30px; height: 30px");
             }
         }
     }
- }
+}
 
- function moveplane() {
+function moveplane() {
     document.addEventListener("keydown", (event) => {
         if ((planeColumn > 0) && (event.key === "ArrowLeft")) {
             --planeColumn;
         }
-        if ((planeColumn < boardWidth - 1) && (event.key === "ArrowRight")) {
+        if ((planeColumn < boardWidth - 1) && (event.key ===
+                "ArrowRight")) {
             ++planeColumn;
         }
     });
-    
- }
 
- function newObstacles() {
+}
+
+function newObstacles() {
     for (let i = 0; i < boardWidth; ++i) {
         matrix[0][i] = 0;
     }
@@ -79,9 +85,9 @@
     matrix[0][currentRandom] = 1;
     currentRandom = Math.floor(Math.random() * (halfWidth + 1));
     matrix[0][currentRandom + halfWidth] = 1;
- }
+}
 
- function lowerObstacles() {
+function lowerObstacles() {
     for (let i = boardHeight - 1; i > 0; --i) {
         for (let j = 0; j < boardWidth; ++j) {
             if (matrix[i][j] === 1) {
@@ -92,9 +98,9 @@
             }
         }
     }
- }
+}
 
- function checkGameOver() {
+function checkGameOver() {
     if (matrix[boardHeight - 1][planeColumn] == 1) {
         const message = document.getElementById("winnerMessage");
         message.innerHTML = "Game Over! Your score is " + score;
@@ -104,14 +110,14 @@
         }
         planeColumn = 3;
     }
- }
+}
 
- function displayScore() {
+function displayScore() {
     const currentScore = document.getElementById("score");
     currentScore.innerHTML = score;
- }
+}
 
- function shoot() {
+function shoot() {
     document.addEventListener("keydown", (event) => {
         if (event.key === "ArrowUp") {
             matrix[boardHeight - 2][planeColumn] = 2;
@@ -135,7 +141,7 @@ function advanceProjectiles() {
     }
 }
 
- function advanceGame() {
+function advanceGame() {
     if (gameOver) {
         gameOver = false;
         resetTable();
@@ -146,17 +152,17 @@ function advanceProjectiles() {
     } else {
         startTime = Date.now();
         let currentId = 0;
-        currentId = setInterval(displayScore ,100);
+        currentId = setInterval(displayScore, 100);
         cyclicalFunctionsId.push(currentId);
-        currentId = setInterval(advanceProjectiles ,100);
+        currentId = setInterval(advanceProjectiles, 100);
         cyclicalFunctionsId.push(currentId);
-        currentId = setInterval(checkGameOver ,50);
+        currentId = setInterval(checkGameOver, 50);
         cyclicalFunctionsId.push(currentId);
-        currentId = setInterval(setColors ,50);
+        currentId = setInterval(setColors, 50);
         cyclicalFunctionsId.push(currentId);
-        currentId = setInterval(lowerObstacles ,1000);
+        currentId = setInterval(lowerObstacles, 1000);
         cyclicalFunctionsId.push(currentId);
-        currentId = setInterval(newObstacles ,1000);
+        currentId = setInterval(newObstacles, 1000);
         cyclicalFunctionsId.push(currentId);
     }
- }
+}
